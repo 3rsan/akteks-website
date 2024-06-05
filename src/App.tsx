2e8ff2f22aa69093465ced9005b88b7aa576f1a0
Cihan Imren <cihan.imren@gmail.com>
2024-06-05 16:07:41 +0300

    default routing, refactoring & minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,22 @@ import About from './pages/about';
 import Contact from './pages/contact';
 import Home from './pages/home';
 import Products from './pages/products';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <HashRouter>
         <ScrollToTop />
         <Navbar></Navbar>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/products" element={<Products />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/products' element={<Products />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+
+          {/* default redirect to home page */}
+          <Route path='*' element={<Navigate to='/' />} />
         </Routes>
         <Footer></Footer>
       </HashRouter>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,4 @@
 import Carousel from '../../components/carousel';
-import FeaturedProducts from '../../components/featured-products';
 import { useTranslation } from 'react-i18next';
 import './styles.css';
 import { Link } from 'react-router-dom';
@@ -7,48 +6,48 @@ import { Link } from 'react-router-dom';
 function Home() {
   const { t: translate } = useTranslation();
   return (
-    <div className="home">
+    <div className='home'>
       <Carousel></Carousel>
-      <section className="about section-padding">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 text-center">
-              <h2 className="mb-5">
+      <section className='about section-padding'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12 text-center'>
+              <h2 className='mb-5'>
                 <span>{translate('get-to-know-1')}</span>{' '}
                 {translate('get-to-know-2')}
               </h2>
             </div>
 
-            <div className="col-lg-10 col-12">
-              <div className="tab-content mt-2" id="pills-tabContent">
+            <div className='col-lg-10 col-12'>
+              <div className='tab-content mt-2' id='pills-tabContent'>
                 <div
-                  className="tab-pane fade show active"
-                  id="pills-home"
-                  role="tabpanel"
-                  aria-labelledby="pills-home-tab"
+                  className='tab-pane fade show active'
+                  id='pills-home'
+                  role='tabpanel'
+                  aria-labelledby='pills-home-tab'
                 >
-                  <div className="row">
-                    <div className="col-lg-7 col-12">
+                  <div className='row'>
+                    <div className='col-lg-7 col-12'>
                       <img
-                        src="images/textile-2.avif"
-                        className="img-fluid"
-                        alt=""
+                        src='images/textile-2.avif'
+                        className='img-fluid'
+                        alt=''
                       />
                     </div>
 
-                    <div className="col-lg-5 col-12">
-                      <div className="d-flex flex-column h-100 ms-lg-4 mt-lg-0 mt-5">
-                        <h4 className="mb-3">
+                    <div className='col-lg-5 col-12'>
+                      <div className='d-flex flex-column h-100 ms-lg-4 mt-lg-0 mt-5'>
+                        <h4 className='mb-3'>
                           <span>{translate('design')}</span> {translate('and')}
                           <span> {translate('manifactured')}</span>
                         </h4>
 
                         <p>{translate('banner-explanation')}</p>
 
-                        <div className="mt-2 mt-lg-auto">
-                          <Link to="/about" className="custom-link mb-2">
+                        <div className='mt-2 mt-lg-auto'>
+                          <Link to='/about' className='custom-link mb-2'>
                             {translate('more-information')}
-                            <i className="bi-arrow-right ms-2"></i>
+                            <i className='bi-arrow-right ms-2'></i>
                           </Link>
                         </div>
                       </div>
@@ -60,33 +59,32 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className="front-product">
-        <div className="container-fluid p-0">
-          <div className="row align-items-center">
-            <div className="col-lg-6 col-12">
+      <section className='front-product'>
+        <div className='container-fluid p-0'>
+          <div className='row align-items-center'>
+            <div className='col-lg-6 col-12'>
               <img
-                src="images/woman-selling-fabrics.jpg"
-                className="img-fluid"
-                alt=""
+                src='images/woman-selling-fabrics.jpg'
+                className='img-fluid'
+                alt=''
               />
             </div>
-            <div className="col-lg-6 col-12">
-              <div className="px-5 py-5 py-lg-0">
-                <h2 className="mb-4">
+            <div className='col-lg-6 col-12'>
+              <div className='px-5 py-5 py-lg-0'>
+                <h2 className='mb-4'>
                   <span>{translate('production')}</span> {translate('and')}{' '}
                   <span>{translate('sales')}</span>
                 </h2>
-                <p className="lead mb-4">{translate('sales-information')}</p>
-                <Link to="/products" className="custom-link">
+                <p className='lead mb-4'>{translate('sales-information')}</p>
+                <Link to='/products' className='custom-link'>
                   {translate('examine-product')}
-                  <i className="bi-arrow-right ms-2"></i>
+                  <i className='bi-arrow-right ms-2'></i>
                 </Link>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <FeaturedProducts></FeaturedProducts>
     </div>
   );
 }
